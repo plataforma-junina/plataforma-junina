94048478bea97cec27eb39b5eaf35bdfd1843ade
Adriano Ferreira <adrianoferreira.dev@gmail.com>
2025-03-10 09:00:39 -0300

    refactor(tests): remove setUp method from TestCase and add Inertia configuration file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,11 +8,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Override the Inertia view-finder configuration
-        config()->set('inertia.testing.view-finder', resource_path('js/pages'));
-    }
+    //
 }
