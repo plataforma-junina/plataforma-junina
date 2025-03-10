6761be755ae4c1350eab6ab12cb0a6434f8ede63
Adriano Ferreira <adrianoferreira.dev@gmail.com>
2025-03-10 08:16:07 -0300

    fix(tests): override inertia.testing.view-finder configuration to resolve GitHub Actions error

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,5 +8,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Override the Inertia view-finder configuration
+        config()->set('inertia.testing.view-finder', resource_path('js/pages'));
+    }
 }
