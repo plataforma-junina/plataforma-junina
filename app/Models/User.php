885e55fc11891f74fa6e7d04e3859921fecccc19
Adriano Ferreira <adrianoferreira.dev@gmail.com>
2025-03-27 20:48:51 -0300

    test: add architectural tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ final class User extends Authenticatable implements MustVerifyEmail
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    public function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
